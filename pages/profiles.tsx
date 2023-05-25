ee828e03aa9aa0cce04205c5bfb483ce25784aca
Maroš Michna <maros24121990@gmail.com>
2023-05-25 13:08:51 +0200

    update

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -20,7 +20,7 @@ export async function getServerSideProps(context: NextPageContext) {
     }
   }
 
-const profiles = () => {
+const Profiles = () => {
 
   const router = useRouter()
   const { data: user } = useCurrentUser()
@@ -70,4 +70,4 @@ const profiles = () => {
   )
 }
 
-export default profiles
+export default Profiles
